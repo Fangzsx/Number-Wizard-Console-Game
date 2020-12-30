8e52e4c19c8168f14837cd39c9e9ece7f26a35b4
Fangszx <jygrzn@gmail.com>
2020-12-30 18:42:12 +0800

    Update NumberWizard.cs

--- Number Wizard Game/Assets/NumberWizard.cs
+++ Number Wizard Game/Assets/NumberWizard.cs
@@ -2,19 +2,24 @@
 
 public class NumberWizard : MonoBehaviour
 {
+
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+
     // Start is called before the first frame update
     void Start()
     {
 
-        int max = 1000;
-        int min = 1;
+        
+
 
         Debug.Log("Welcome to Number Wizard Game!");
         Debug.Log("Please pick a number from the following");
         Debug.Log("Highest number you can pick is " + max);
         Debug.Log("Lowest number possible is " + min);
         Debug.Log("Tell me if your number is higher or lower than 500.");
-        Debug.Log("Push up = Higher, Push down = Lower, Push Enter = Correct");
+        Debug.Log("Push up/ W key = Higher, Push down/ W key = Lower, Push Enter = Correct");
 
 
     }
@@ -22,17 +27,27 @@ public class NumberWizard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
-            Debug.Log("Up arrow was pressed!");
+            Debug.Log("Up arrow/W key was pressed!");
+            min = guess;
+            Debug.Log(guess);
+
         }
-        if(Input.GetKeyDown(KeyCode.DownArrow))
+
+        else if(Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
         {
-            Debug.Log("Down arrow was pressed!");
+            Debug.Log("Down arrow/S key was pressed!");
+            max = guess;
+            Debug.Log(guess);
         }
-        if(Input.GetKeyDown(KeyCode.Return))
+
+        else if(Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Enter was pressed!");
         }
+
+
+
     }
 }
